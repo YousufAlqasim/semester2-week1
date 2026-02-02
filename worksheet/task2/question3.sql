625807d316e25f0bb85ae16325b3ba9e0dbeb7c3
Yousuf Abdulrahman Yousif Abdulla Alqasim <zwgf0500@leeds.ac.uk>
2026-02-02 17:46:21 +0000

    ds

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -2,5 +2,5 @@
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
 
-SELECT d.DepartmentName, COUNT(e.EnrolmentID) AS TotalEnrolments FROM department d JOIN course c ON d.DepartmentID = e.CourseID
+SELECT d.DepartmentName, COUNT(e.EnrolmentID) AS TotalEnrolments FROM department d JOIN course c ON d.DepartmentID = c.DepartmentID
 Join enrolment e  ON c.CourseID = e.CourseID GROUP BY d.DepartmentName;
